34e493bfe04be289d966c1081c303ee9c3ed3510
Jerry Yang <hy0kle@gmail.com>
2015-01-27 15:41:30 +0800

    完善控制脚本

--- data/control.sh
+++ data/control.sh
@@ -9,10 +9,14 @@
 # 1. 执行预处理
 ./pretreatment.sh
 
+# 2. 学习
+svm-train svm-org model_file
+
+# 3. 遍历蓝球
 blue_ball="01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16"
 
-# 2. 遍历蓝球
 for blue in $blue_ball
 do
+    # 4. 预测
     nohup ./predict.sh $blue &
 done
